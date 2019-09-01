9a94ad4b2d1231c6372f27bee9b48d2416a8657d
Alan <1060908200@qq.com>
2019-09-01 11:42:23 +0800

    initial commit by l

---
+++ a.java
@@ -0,0 +1,12 @@
+  public static void main(String[] args) {
+	  
+        System.out.println("aaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaa");
+		System.out.println("bbbbbb");
+		System.out.println("bbbbbb");
+		System.out.println("bbbbbb");
+		System.out.println("bbbbbb");
+		
+    }
